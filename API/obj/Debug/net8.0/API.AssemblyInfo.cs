c1ce25c5b6f861376377314da76ca28f6e5da902
GirayTurker <grytrk@gmail.com>
2024-03-18 23:39:42 -0400

    EntityFramework - Design/Sqlite

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c3f4dd4cb02d3e7fdae138d984d37b90113a14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12cbecb6c71e78365611b2cc997fb138ee50b7fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
