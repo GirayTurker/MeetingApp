82fedb92ca38382f6b932197dd3b7d9beaeb0cc3
GirayTurker <grytrk@gmail.com>
2024-03-23 08:06:07 -0400

    Routing in code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit{
   model: any = {}
 
-  constructor(public accountService:AccountService){}
+  constructor(public accountService:AccountService, private router:Router){}
 
   ngOnInit(): void {
    
@@ -31,13 +32,15 @@ export class NavComponent implements OnInit{
       next: response => {
         console.log(response);
         //this.loggedIn=true;
+        this.router.navigateByUrl('/members')
       },
-      error: error=>console.log(error),
+      error: error=>console.log(error)
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
     //this.loggedIn = false;
   }
 
