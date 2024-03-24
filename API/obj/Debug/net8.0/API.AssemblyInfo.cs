a3669276fcf475c93c305f4d33a075b3209361d3
GirayTurker <grytrk@gmail.com>
2024-03-23 20:58:01 -0400

    Prev commit with first letter capitialize

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ba1d19ca5f63bd1d9be5fae6747edcfae11054")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2eff4e1e1e1eae7e04579962a4212bde42b975a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
