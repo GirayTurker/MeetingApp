afb32054f18e1a91549736f8015a0e42b8546461
GirayTurker <grytrk@gmail.com>
2024-03-31 22:46:14 -0400

    User entity API extension #AppUser class new propreties #Photo class created

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a19294336ff7f371ca91a13b1a46c37ccc6fce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef637b6d0e0475a55af5a4dd4358eb8a9c22464a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
