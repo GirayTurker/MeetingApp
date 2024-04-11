d6c89725a9d0f814f66eb7e2b10d99693f210bc2
GirayTurker <grytrk@gmail.com>
2024-04-11 16:27:55 -0400

    TypeScript Part # Demo to how to use

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -3,3 +3,33 @@ export interface IUser{
     username: string;
     token: string;
 }
+
+
+//===================== IMPORTANT DEMO FOR TYPESCRIPT!!!!!
+
+// let data: number | string = 42;
+
+// data = "10";
+
+// interface Car{
+//     color: string,
+//     model:string,
+//     topSpeed?:number
+// }
+
+// const car1:Car ={
+//     color: 'blue',
+//     model: 'BMW'
+// } 
+
+// const car2:Car ={
+//     color: 'red',
+//     model: 'Mercedes',
+//     topSpeed:100
+// } 
+
+// const multiply = (x:number, y:number):number =>{
+//     return x*y;
+// }
+
+//===================== IMPORTANT DEMO FOR TYPESCRIPT!!!!!
